65c99e620fbac9b1ee62c41ead59b17239c01c0b
Mallikarjun Padanad <145246181+MallikarjunPadanad@users.noreply.github.com>
2026-08-24 18:58:19 +0530

    Time: 10 ms (66.06%), Space: 45.8 MB (51.58%) - LeetHub

--- 2376-count-special-integers/2376-count-special-integers.java
+++ 2376-count-special-integers/2376-count-special-integers.java
@@ -1,75 +1,65 @@
+import java.util.*;
+
 class Solution {
 
-    int[][][][] dp = new int[10][2][2][1024];
+    int[][][] dp;
+    String num;
 
-    int solve(int index, int tight, int started, int mask, String num) {
+    int solve(int index, int tight, int mask) {
 
-        // Base case
         if (index == num.length()) {
-            return started == 1 ? 1 : 0;
+            return mask == 0 ? 0 : 1;
         }
 
-        // Already calculated
-        if (dp[index][tight][started][mask] != -1) {
-            return dp[index][tight][started][mask];
+        if (dp[index][tight][mask] != -1) {
+            return dp[index][tight][mask];
         }
 
-        int limit = (tight == 1)
+        int limit = tight == 1
                 ? num.charAt(index) - '0'
                 : 9;
 
-        int answer = 0;
+        int ans = 0;
 
         for (int digit = 0; digit <= limit; digit++) {
 
-            // IMPORTANT:
-            // Use original tight value, not limit
-            int newTight = (tight == 1 && digit == limit) ? 1 : 0;
+            int newTight =
+                    (tight == 1 && digit == limit) ? 1 : 0;
 
             // Leading zero
-            if (started == 0 && digit == 0) {
-
-                answer += solve(
-                        index + 1,
-                        newTight,
-                        0,
-                        mask,
-                        num
-                );
-
-            } else {
+            if (mask == 0 && digit == 0) {
+                ans += solve(index + 1, newTight, 0);
+            }
+            else {
 
                 // Digit already used
                 if ((mask & (1 << digit)) != 0) {
                     continue;
                 }
 
-                answer += solve(
+                ans += solve(
                         index + 1,
                         newTight,
-                        1,
-                        mask | (1 << digit),
-                        num
+                        mask | (1 << digit)
                 );
             }
         }
 
-        return dp[index][tight][started][mask] = answer;
+        return dp[index][tight][mask] = ans;
     }
 
     public int countSpecialNumbers(int n) {
 
-        String num = String.valueOf(n);
+        num = String.valueOf(n);
 
-        // Fill DP with -1
-        for (int i = 0; i < 10; i++) {
+        dp = new int[num.length()][2][1024];
+
+        for (int i = 0; i < num.length(); i++) {
             for (int j = 0; j < 2; j++) {
-                for (int k = 0; k < 2; k++) {
-                    java.util.Arrays.fill(dp[i][j][k], -1);
-                }
+                Arrays.fill(dp[i][j], -1);
             }
         }
 
-        return solve(0, 1, 0, 0, num);
+        return solve(0, 1, 0);
     }
 }
