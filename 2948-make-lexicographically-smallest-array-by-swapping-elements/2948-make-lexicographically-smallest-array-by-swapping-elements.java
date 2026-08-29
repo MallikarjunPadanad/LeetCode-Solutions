f70b5ddf33240cf4911ac4ae5d83c4b1e548222a
Mallikarjun Padanad <145246181+MallikarjunPadanad@users.noreply.github.com>
2026-08-29 10:23:41 +0530

    Time: 63 ms (97.73%), Space: 140.9 MB (80.3%) - LeetHub

--- 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.java
+++ 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.java
@@ -2,33 +2,29 @@ class Solution {
   public int[] lexicographicallySmallestArray(int[] nums, int limit) {
     int n = nums.length;
     int[] ans = new int[n];
-    Pair<Integer, Integer>[] numAndIndexes = getNumAndIndexes(nums);
+    Integer[] order = new Integer[n];
+    for (int i = 0; i < n; ++i)
+      order[i] = i;
+    Arrays.sort(order, (a, b) -> nums[a] - nums[b]);
 
     int i = 0;
     while (i < n) {
       int j = i;
-      while (j + 1 < n && numAndIndexes[j + 1].getKey() - numAndIndexes[j].getKey() <= limit)
-        j++;
+      while (j + 1 < n && nums[order[j + 1]] - nums[order[j]] <= limit)
+        ++j;
 
-      List<Integer> sortedIndices = new ArrayList<>();
-      for (int k = i; k <= j; k++)
-        sortedIndices.add(numAndIndexes[k].getValue());
-      Collections.sort(sortedIndices);
+      int size = j - i + 1;
+      int[] sortedIndices = new int[size];
+      for (int k = 0; k < size; ++k)
+        sortedIndices[k] = order[i + k];
+      Arrays.sort(sortedIndices);
 
-      for (int k = 0; k <= j - i; k++)
-        ans[sortedIndices.get(k)] = numAndIndexes[i + k].getKey();
+      for (int k = 0; k < size; ++k)
+        ans[sortedIndices[k]] = nums[order[i + k]];
 
       i = j + 1;
     }
 
     return ans;
-  }
-
-  private Pair<Integer, Integer>[] getNumAndIndexes(int[] nums) {
-    Pair<Integer, Integer>[] numAndIndexes = new Pair[nums.length];
-    for (int i = 0; i < nums.length; ++i)
-      numAndIndexes[i] = new Pair<>(nums[i], i);
-    Arrays.sort(numAndIndexes, Comparator.comparingInt(Pair::getKey));
-    return numAndIndexes;
   }
 }
