b0d91edca0e04fd35e3e8b878a23ffdef8a86288
Mallikarjun Padanad <145246181+MallikarjunPadanad@users.noreply.github.com>
2026-09-25 09:50:26 +0530

    Time: 8 ms (92.8%), Space: 46.8 MB (90.4%) - LeetHub

--- 1096-brace-expansion-ii/1096-brace-expansion-ii.java
+++ 1096-brace-expansion-ii/1096-brace-expansion-ii.java
@@ -1,60 +1,44 @@
 class Solution {
-    private String s;
-    private int pos;
+  public List<String> braceExpansionII(String expression) {
+    return dfs(expression, 0, expression.length() - 1);
+  }
 
-    public List<String> braceExpansionII(String expression) {
-        this.s = expression;
-        this.pos = 0;
-        Set<String> result = parseExpr();
-        List<String> list = new ArrayList<>(result);
-        Collections.sort(list);
-        return list;
+  private List<String> dfs(final String expression, int s, int e) {
+    TreeSet<String> ans = new TreeSet<>();
+    List<List<String>> groups = new ArrayList<>();
+    groups.add(new ArrayList<>());
+    int layer = 0;
+    int left = 0;
+
+    for (int i = s; i <= e; ++i)
+      if (expression.charAt(i) == '{' && ++layer == 1)
+        left = i + 1;
+      else if (expression.charAt(i) == '}' && --layer == 0)
+        merge(groups, dfs(expression, left, i - 1));
+      else if (expression.charAt(i) == ',' && layer == 0)
+        groups.add(new ArrayList<>());
+      else if (layer == 0)
+        merge(groups, new ArrayList<>(List.of(String.valueOf(expression.charAt(i)))));
+
+    for (final List<String> group : groups)
+      for (final String word : group)
+        ans.add(word);
+
+    return new ArrayList<>(ans);
+  }
+
+  void merge(List<List<String>> groups, List<String> group) {
+    if (groups.get(groups.size() - 1).isEmpty()) {
+      groups.set(groups.size() - 1, group);
+      return;
     }
 
-    // expr := term (',' term)*
-    private Set<String> parseExpr() {
-        Set<String> result = new TreeSet<>(parseTerm());
-        while (pos < s.length() && s.charAt(pos) == ',') {
-            pos++; // skip ','
-            result.addAll(parseTerm());
-        }
-        return result;
-    }
+    List<String> mergedGroup = new ArrayList<>();
 
-    // term := factor+   (Cartesian-product concatenation)
-    private Set<String> parseTerm() {
-        List<Set<String>> factors = new ArrayList<>();
-        while (pos < s.length() && s.charAt(pos) != ',' && s.charAt(pos) != '}') {
-            factors.add(parseFactor());
-        }
-        Set<String> result = new HashSet<>();
-        result.add("");
-        for (Set<String> factor : factors) {
-            Set<String> next = new HashSet<>();
-            for (String prefix : result) {
-                for (String suffix : factor) {
-                    next.add(prefix + suffix);
-                }
-            }
-            result = next;
-        }
-        return result;
-    }
+    for (final String word1 : groups.get(groups.size() - 1))
+      for (final String word2 : group)
+        mergedGroup.add(word1 + word2);
 
-    // factor := letters | '{' expr '}'
-    private Set<String> parseFactor() {
-        Set<String> result = new HashSet<>();
-        if (s.charAt(pos) == '{') {
-            pos++; // skip '{'
-            result = parseExpr();
-            pos++; // skip '}'
-        } else {
-            int start = pos;
-            while (pos < s.length() && Character.isLetter(s.charAt(pos))) {
-                pos++;
-            }
-            result.add(s.substring(start, pos));
-        }
-        return result;
-    }
+    groups.set(groups.size() - 1, mergedGroup);
+  }
 }
